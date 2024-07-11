c2859143a87263b5cc32e58636e83847b4bda71d
yannipeng <yannipeng@google.com>
2024-07-11 12:28:43 -0500

    remove service annotation

--- src/main/java/com/example/quotes/domain/GenerateQuote.java
+++ src/main/java/com/example/quotes/domain/GenerateQuote.java
@@ -26,7 +26,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
-@Service
 public class GenerateQuote {
 
 	private VertexAiGeminiChatModel chatClient;
